64e388159cf352694101bd715ed6e99e57f6a5d4
szpatis21 <szpatis21@gmail.com>
2023-06-04 11:33:32 +0200

    -

---
+++ dist/proba.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function myFunction() {
+  var checkBox = document.getElementById('myCheck , myChecki');
+  var text = document.getElementById('text , texti');
+
+  if (checkBox.checked == true) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
+
+function myFunctionii() {
+  var checkBox = document.getElementById("myCheckii");
+  var text = document.getElementById("textii");
+
+  if (checkBox.checked == true) {
+    text.style.display = "block";
+  } else {
+    text.style.display = "none";
+  }
+}
---
+++ proba.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var checkBox = document.getElementById('myCheck , myChecki');
+    var text = document.getElementById('text , texti');
+
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+  }
+
+  
+  function myFunctionii() {
+    var checkBox = document.getElementById("myCheckii");
+    var text = document.getElementById("textii");
+    
+    if (checkBox.checked == true){
+      text.style.display = "block";
+    } else {
+       text.style.display = "none";
+    }
+  }
